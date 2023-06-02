59c805c75a162fdfef594a4ffab96cb10fe2428f
TamTLeader <21521408@gm.uit.edu.vn>
2023-06-02 10:50:59 +0700

    update admin UI, fix some bug, add drawable img and xml

--- app/src/main/java/com/example/fashop/activity/AdminMainActivity.java
+++ app/src/main/java/com/example/fashop/activity/AdminMainActivity.java
@@ -6,9 +6,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import com.example.fashop.R;
@@ -19,13 +22,13 @@ import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.util.Collections;
 import java.util.HashMap;
 
 public class AdminMainActivity extends AppCompatActivity {
 
     private ImageButton logoutBtn;
-    Button buttonPedit,buttonPadd;
-    Button btnAddCategory, btnEditCategory;
+    ImageButton buttonPedit,buttonPadd,btnAddCategory, btnEditCategory;
     private FirebaseAuth firebaseAuth;
     private ProgressDialog progressDialog;
     @Override
@@ -89,7 +92,6 @@ public class AdminMainActivity extends AppCompatActivity {
             }
         });
     }
-
     private void makeMeOffline() {
         //after logging in, make user online
         progressDialog.setMessage("Logging Out...");
