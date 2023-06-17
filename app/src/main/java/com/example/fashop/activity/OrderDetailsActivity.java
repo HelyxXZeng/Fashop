8ee1ef7a0a7e258de54558b2fec48eb48ac0210b
TamTLeader <21521408@gm.uit.edu.vn>
2023-06-17 21:30:47 +0700

    done code backend of order manager

--- app/src/main/java/com/example/fashop/activity/OrderDetailsActivity.java
+++ app/src/main/java/com/example/fashop/activity/OrderDetailsActivity.java
@@ -1,14 +1,21 @@
 package com.example.fashop.activity;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.ProgressDialog;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -18,23 +25,31 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import Adapter.OrderItemAdapter;
 import Model.CartItem;
+import Model.ModelImage;
 import Model.Order;
 import Model.OrderItem;
+import Model.ProductModel;
 
 public class OrderDetailsActivity extends AppCompatActivity {
     TextView un, orderid, cd, address, nob, ordervalue;
     OrderItemAdapter adapter;
     RecyclerView OrderItemList;
     List<OrderItem> orderItems = new ArrayList<>();
-    Button confirmbtn,denybtn;
+    Button confirmbtn;
     ImageButton backbtn;
+    Spinner spinner;
+    ProgressDialog progressDialog;
+    String selectedItem;
     Order order;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,9 +63,8 @@ public class OrderDetailsActivity extends AppCompatActivity {
         nob = findViewById(R.id.note);
         ordervalue = findViewById(R.id.totalCost);
         OrderItemList = findViewById(R.id.order_item_rcv);
-
+        spinner = findViewById(R.id.statusSpinner);
         confirmbtn = findViewById(R.id.confirm_button);
-        denybtn = findViewById(R.id.deny_button);
         backbtn = findViewById(R.id.backBtn);
 
         backbtn.setOnClickListener(v -> { finish();});
@@ -61,6 +75,10 @@ public class OrderDetailsActivity extends AppCompatActivity {
     {
         order = (Order) getIntent().getSerializableExtra("order");
         orderItems.clear();
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(OrderDetailsActivity.this, LinearLayoutManager.VERTICAL, false);
+        OrderItemList.setLayoutManager(linearLayoutManager);
+        adapter = new OrderItemAdapter(orderItems);
+        OrderItemList.setAdapter(adapter);
         DatabaseReference ref = FirebaseDatabase.getInstance().getReference("Users");
         ref.orderByChild("uid").equalTo(order.getCustomerID()).addValueEventListener(new ValueEventListener() {
             @Override
@@ -95,7 +113,7 @@ public class OrderDetailsActivity extends AppCompatActivity {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
                 for(DataSnapshot ds: snapshot.getChildren()){
-                    OrderItem orderItem =  new OrderItem(ds.getValue(OrderItem.class));
+                    OrderItem orderItem =  ds.getValue(OrderItem.class);
 
                     ref3.orderByChild("id").equalTo(orderItem.getVariantID()).addValueEventListener(new ValueEventListener() {
                         @Override
@@ -104,22 +122,48 @@ public class OrderDetailsActivity extends AppCompatActivity {
                             {
                                 orderItem.setColor(hm.child("color").getValue().toString());
                                 orderItem.setSize(hm.child("size").getValue().toString());
-
+                                adapter.notifyDataSetChanged();
                                 DatabaseReference reference = FirebaseDatabase.getInstance().getReference("ModelImage");
-                                reference.orderByChild("modelID").equalTo(hm.child("modelID").getValue().toString()).addValueEventListener(new ValueEventListener() {
+
+
+                                Query query2 = reference.orderByChild("modelID").equalTo(Integer.parseInt(hm.child("modelID").getValue().toString()));
+                                query2.addListenerForSingleValueEvent(new ValueEventListener() {
                                     @Override
-                                    public void onDataChange(@NonNull DataSnapshot snapshot) {
-                                        for(DataSnapshot e: snapshot.getChildren()) {
-                                            Log.v("test", e.child("url").getValue().toString());
-                                            orderItem.setImage(e.child("url").getValue().toString());
+                                    public void onDataChange(DataSnapshot dataSnapshot) {
+                                        for (DataSnapshot modelSnapshot : dataSnapshot.getChildren()) {
+                                            ModelImage image = modelSnapshot.getValue(ModelImage.class);
+                                            orderItem.setImage(image.getUrl());
+                                            adapter.notifyDataSetChanged();
+                                            break;
                                         }
                                     }
 
                                     @Override
-                                    public void onCancelled(@NonNull DatabaseError error) {
-
+                                    public void onCancelled(DatabaseError databaseError) {
+                                        // Handle errors here
                                     }
                                 });
+
+                                DatabaseReference ref4 = FirebaseDatabase.getInstance().getReference("Model");
+
+                                Query query3 = ref4.orderByChild("id").equalTo(Integer.parseInt(hm.child("modelID").getValue().toString()));
+                                query3.addListenerForSingleValueEvent(new ValueEventListener() {
+                                    @Override
+                                    public void onDataChange(DataSnapshot dataSnapshot) {
+                                        for (DataSnapshot modelSnapshot : dataSnapshot.getChildren()) {
+                                            ProductModel m = modelSnapshot.getValue(ProductModel.class);
+                                            orderItem.setPrice(m.getPrice());
+                                            adapter.notifyDataSetChanged();
+                                            break;
+                                        }
+                                    }
+
+                                    @Override
+                                    public void onCancelled(DatabaseError databaseError) {
+                                        // Handle errors here
+                                    }
+                                });
+
                             }
 
                         }
@@ -130,18 +174,58 @@ public class OrderDetailsActivity extends AppCompatActivity {
                         }
                     });
                     orderItems.add(orderItem);
+                    adapter.notifyDataSetChanged();
                 }
 
-                LinearLayoutManager linearLayoutManager = new LinearLayoutManager(OrderDetailsActivity.this, LinearLayoutManager.VERTICAL, false);
-                OrderItemList.setLayoutManager(linearLayoutManager);
-                adapter = new OrderItemAdapter(orderItems);
-                OrderItemList.setAdapter(adapter);
+
 
             }
 
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
 
+            }
+        });
+
+        ArrayList<String> status = new ArrayList<>();
+        status.add("PENDING");
+        status.add("CONFIRMED");
+        status.add("SHIPPING");
+        status.add("COMPLETED");
+        status.add("DECLINED");
+        ArrayAdapter<String> statusadapter = new ArrayAdapter<>(OrderDetailsActivity.this,
+                R.layout.spinner_gray_layout, status);
+        spinner.setAdapter(statusadapter);
+        spinner.setSelection(statusadapter.getPosition(order.getStatus()));
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                selectedItem = spinner.getSelectedItem().toString();
+                statusadapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        confirmbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                DatabaseReference setref = FirebaseDatabase.getInstance().getReference("Order");
+                order.setStatus(selectedItem);
+                setref.child(String.valueOf(order.getID())).setValue(order,
+                        new DatabaseReference.CompletionListener(){
+                            @Override
+                            public void onComplete(@Nullable DatabaseError error, @NonNull DatabaseReference ref)
+                            {
+                                Toast.makeText(getApplicationContext(), "Data saved successfully", Toast.LENGTH_SHORT).show();
+                                finish();
+                            }
+
+                        });
             }
         });
     }
